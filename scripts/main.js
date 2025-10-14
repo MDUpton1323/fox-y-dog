9e7ef51d37137fb5154bfcc2baac6e3c01786c9f
Marcus Upton <MarcusDUpton13@gmail.com>
2025-10-14 02:41:30 -0500

    Added fetch to both eventListners

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const foxButton = document.querySelector("#fox-button");
+foxButton.addEventListener("click", () => {
+  fetch("https://randomfox.ca/floof/");
+});
+
+const dogButton = document.querySelector("#dog-button");
+dogButton.addEventListener("click", () => {
+  fetch("https://random.dog/woof.json");
+});
