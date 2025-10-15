7cd31bda9a6c5dfbaac26c0875152986275d251c
Marcus Upton <MarcusDUpton13@gmail.com>
2025-10-14 21:18:41 -0500

    using .then method and async/await method's to complete project

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,29 @@
 const foxButton = document.querySelector("#fox-button");
+const foxImage = document.querySelector("#fox");
 foxButton.addEventListener("click", () => {
-  fetch("https://randomfox.ca/floof/");
+  fetch("https://randomfox.ca/floof/")
+    .then((response) => {
+      console.log("Response received!");
+      // Parse the JSON in the response
+      return response.json();
+    })
+    .then((foxData) => {
+      // This code runs when the JSON parsing is complete
+      console.log("Data parsed!");
+      console.log(foxData);
+      foxImage.src = foxData.image;
+    });
+
+  console.log("This code runs immediately!");
 });
 
 const dogButton = document.querySelector("#dog-button");
-dogButton.addEventListener("click", () => {
-  fetch("https://random.dog/woof.json");
-});
+const dogImage = document.querySelector("#dog");
+const displayDogImage = async () => {
+  const response = await fetch("https://random.dog/woof.json");
+  const dogData = await response.json();
+  const dogImage = document.querySelector("#dog");
+  dogImage.src = dogData.url;
+};
+
+dogButton.addEventListener("click", displayDogImage);
